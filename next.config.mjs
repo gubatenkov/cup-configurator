d88b4432c2750053436527a3bd4f5235d4e9a0a9
Slava <Slava@macbook.local>
2024-02-13 16:35:41 +0200

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    config.externals = [...config.externals, "canvas"]
+    return config
+  }
 };
 
 export default nextConfig;
