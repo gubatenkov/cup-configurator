56badd2729b018fb5c9f3e53f24e43fb8208df30
Slava <Slava@macbook.local>
2024-02-13 17:34:23 +0200

    changes in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // experimental: {
-  //   outputFileTracingExcludes: {
-  //     'canvas': ['**canvas**']
-  //   }
-  // },
-  // webpack: (config) => {
-  //   config.externals = [...config.externals, "canvas", "jsdom"]
-  //   return config
-  // }
+  experimental: {
+    outputFileTracingExcludes: {
+      'canvas': ['**canvas**']
+    }
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, "canvas", "jsdom"]
+    return config
+  }
 };
 
 export default nextConfig;
