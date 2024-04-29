91cce16098f5b707ffe214ed01e599f702d9bba3
Slava <Slava@macbook.local>
2024-04-29 20:19:18 +0300

    edit next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+      canvas: 'commonjs canvas',
+    })
+    return config
+  },
   reactStrictMode: false,
 }
 
